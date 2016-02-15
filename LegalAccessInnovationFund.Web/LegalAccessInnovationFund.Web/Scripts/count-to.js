d17be04a0af5661ef7e5147e09484b4465078923
Justin Wyatt <justinjwyatt@hotmail.com>
2016-02-15 17:21:00 -0600

    Added Froala wysiwg

---
+++ LegalAccessInnovationFund.Web/LegalAccessInnovationFund.Web/Scripts/count-to.js
@@ -0,0 +1,80 @@
+﻿Skip to content
+This repository  
+Search
+Pull requests
+Issues
+Gist
+ @JustinWyatt
+Watch 2
+Star 98
+Fork 61 sparkalow/angular-count-to
+Code  Issues 6  Pull requests 7  Wiki  Pulse  Graphs
+Branch: master Find file Copy pathangular-count-to/src/count-to.js
+4c5beef  on Sep 11, 2014
+@michalgm michalgm Update count-to.js
+1 contributor
+RawBlameHistory     63 lines (52 sloc)  2.04 KB
+var countTo = angular.module('countTo', [])
+    .directive('countTo', ['$timeout', function ($timeout) {
+        return {
+            replace: false,
+            scope: true,
+            link: function (scope, element, attrs) {
+
+                var e = element[0];
+                var num, refreshInterval, duration, steps, step, countTo, value, increment;
+
+                var calculate = function () {
+                    refreshInterval = 30;
+                    step = 0;
+                    scope.timoutId = null;
+                    countTo = parseInt(attrs.countTo) || 0;
+                    scope.value = parseInt(attrs.value, 10) || 0;
+                    duration = (parseFloat(attrs.duration) * 1000) || 0;
+
+                    steps = Math.ceil(duration / refreshInterval);
+                    increment = ((countTo - scope.value) / steps);
+                    num = scope.value;
+                }
+
+                var tick = function () {
+                    scope.timoutId = $timeout(function () {
+                        num += increment;
+                        step++;
+                        if (step >= steps) {
+                            $timeout.cancel(scope.timoutId);
+                            num = countTo;
+                            e.textContent = countTo;
+                        } else {
+                            e.textContent = Math.round(num);
+                            tick();
+                        }
+                    }, refreshInterval);
+
+                }
+
+                var start = function () {
+                    if (scope.timoutId) {
+                        $timeout.cancel(scope.timoutId);
+                    }
+                    calculate();
+                    tick();
+                }
+
+                attrs.$observe('countTo', function (val) {
+                    if (val) {
+                        start();
+                    }
+                });
+
+                attrs.$observe('value', function (val) {
+                    start();
+                });
+
+                return true;
+            }
+        }
+
+    }]);
+Status API Training Shop Blog About Pricing
+© 2016 GitHub, Inc. Terms Privacy Security Contact Help
